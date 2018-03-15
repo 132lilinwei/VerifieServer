1f5587d076a8c8617c5813fe2c0165b5bc83d02d
Linwei <llw19970903@live.com>
2018-03-15 20:52:10 +0800

    add heroku dependencies

--- newsite/settings.py
+++ newsite/settings.py
@@ -135,3 +135,8 @@ EMAIL_HOST_PASSWORD = '132lilinwei'
 EMAIL_PORT = 587
 
 #SENDSMS_BACKEND = 'newsite.mysmsbackend.SmsBackend'
+
+
+# Configure Django App for Heroku.
+import django_heroku
+django_heroku.settings(locals())
