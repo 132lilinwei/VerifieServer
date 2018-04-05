8ae24d6747a38efecb8a6a442e3d84a2f49df5ce
Linwei <llw19970903@live.com>
2018-04-05 22:52:18 +0800

    admin

--- realapp/admin.py
+++ realapp/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from realapp.models import MyUser
 # Register your models here.
-admin.site.register(MyUser)
 
-class PostCodesAdmin(admin.ModelAdmin):
-    exclude = ('pcname',)
+
+class MyAdmin(admin.ModelAdmin):
+    exclude = ('password',)
+
+admin.site.register(MyUser,MyAdmin)
